3601cb78b63857e5078e6e7413a52ab827fa1dd2
anish0905 <anishanish0102@gmail.com>
2024-04-17 16:38:07 +0530

    question 4 completed

---
+++ q5.js
@@ -0,0 +1,17 @@
+Day5>
+
+//*-
+//* Programming Question: Sort an Array
+//*
+5 //? Write a function to sort an array of numbers in an ascending order.
+6
+// Example usage:
+8 // console.log(sortAscending ([5, 3, 1, 8])); // Output: [1, 3, 5, 8]
+// 9 console.log(sortAscending ([5, 3, 10, 8])); // Output: [3, 5, 8,10]
+// 10 console.log(typeof sortAscending [2]);
+11
+12 //todo Requirements:
+13 //? The function should take an array of numbers as input.
+14 //? It should return a new array with the numbers sorted in ascending order.
+15 //7 The original array should remain unchanged.
+16 //? You are not allowed to use the built-in sort() method.
