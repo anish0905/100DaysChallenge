51367d93aca252301c5ee230fab30cd29836da1a
anish0905 <anishanish0102@gmail.com>
2024-04-20 12:26:13 +0530

    Question 9 completed

--- q7.js
+++ q7.js
@@ -1,3 +1,21 @@
 
-
 1 // Write a function findMax that takes an array of numbers as input and returns the maximum number in the array.
+
+//Example  usage:
+
+// console.log(findMax([1,5,3,9,2]));  //output 9
+
+// console.log(findMax([-10,-5,-3,-9,-2]));  //output -2
+
+// console.log(findMax([5]));  // output:5;
+
+
+const findMax =(arr)=>{
+    console.log(...arr);
+    return Math.max(...arr);
+}
+console.log(findMax([5]));
+
+// console.log(findMax([1,5,3,9,2])); 
+// console.log(findMax([-10,-5,-3,-9,-2])); 
+
---
+++ q9.js
@@ -0,0 +1,27 @@
+1 // Challenge: Calculate the Average
+2
+3 // Write a function called calculateAverage that takes an array of numbers as input and returns the average of those numbers.
+4
+5 // Your function should:
+6
+7 // Accept an array of numbers as input.
+8 // Calculate the sum of all the numbers in the array.
+9 // Divide the sum by the total number of elements in the array to find the average.
+10 // Return the calculated average
+// console.log(calculateAverage([4,10,2,8]));   //output 6
+
+
+
+function calculateAverage(no)
+{
+    let sum = 0;
+    for(let i = 0 ; i<no.length ; i++)
+    {
+     
+        sum =sum+no[i]
+    }
+    return sum/no.length;
+
+}
+
+console.log(calculateAverage([4,10,2,8]));
